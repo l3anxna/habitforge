ac239143112d0929fb4983db5fdf9febd36746fd
l3anxna <patthadonaroonpairodjanakul@gmail.com>
2026-03-02 13:52:10 +0700

    REFACTOR: Use controller for home page

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,5 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', \App\Http\Controllers\HomeController::class)->name('home');
