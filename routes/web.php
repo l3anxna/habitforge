2e8c68cc556231725b5892a7d5f043f138c63779
l3anxna <patthadonaroonpairodjanakul@gmail.com>
2026-03-04 14:39:23 +0700

    REFACTOR: Reroute from using UserController to DashboardController

--- routes/web.php
+++ routes/web.php
@@ -4,8 +4,8 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\AdminController;
-use App\Http\Controllers\UserController;
 use App\Http\Controllers\HabitController;
+use App\Http\Controllers\DashboardController;
 
 /*
  * Public routes
